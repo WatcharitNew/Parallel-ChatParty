998f369610e89251ce6db8f0990e5b14e459a1ab
karnkittik <karnkitti_tang@hotmail.com>
2020-04-11 01:26:39 +0700

    Create ChatRoom

--- frontend/src/component/Chat.js
+++ frontend/src/component/Chat.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import "./Chat.css";
 import ChatMessage from "./ChatMessage";
+import ChatRoom from "./ChatRoom";
 export default class Chat extends Component {
   render() {
     return (
       <div className="Chat">
         <div className="Chat-header">
-          <div className="Chat-chatroom-card">
-            <div className="Chat-chatroom-title">Chatroom</div>
-          </div>
+          <ChatRoom />
           <ChatMessage />
         </div>
       </div>
