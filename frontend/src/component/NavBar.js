ae864193e2335ef252527416848c26f4dcec6a66
karnkittik <karnkitti_tang@hotmail.com>
2020-04-12 20:21:17 +0700

    Add nav bar

---
+++ frontend/src/component/NavBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import IconButton from "@material-ui/core/IconButton";
+import "./NavBar.css";
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className="NavBar-tab">
+        <div className="NavBar-title">ChatRoom</div>
+        <div className="NavBar-userName">Karnkittik</div>
+        <IconButton
+          className="NavBar-logOut"
+          onClick={(e) => {
+            alert("Logout");
+          }}
+          color="inherit"
+        >
+          <ExitToAppIcon />
+        </IconButton>
+      </div>
+    );
+  }
+}
