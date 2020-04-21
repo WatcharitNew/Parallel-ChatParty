7053292344a612284b4419f0e4539969cd27a1ec
karnkittik <karnkitti_tang@hotmail.com>
2020-04-21 13:43:23 +0700

    Display username on navbar

--- frontend/src/component/NavBar.js
+++ frontend/src/component/NavBar.js
@@ -4,12 +4,15 @@ import Toolbar from "@material-ui/core/Toolbar";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import IconButton from "@material-ui/core/IconButton";
 import "./NavBar.css";
+import LocalStorageService from "../LocalStorageService";
 export default class NavBar extends Component {
   render() {
     return (
       <div className="NavBar-tab">
         <div className="NavBar-title">ChatRoom</div>
-        <div className="NavBar-userName">Karnkittik</div>
+        <div className="NavBar-userName">
+          {LocalStorageService.getUserName()}
+        </div>
         <IconButton
           className="NavBar-logOut"
           onClick={(e) => {
